4e4473e17625ea5ae74a99c17f5a73a7ddc99bb3
Wtorres27 <costellobenida@gmail.com>
2023-10-14 17:59:43 -0700

    first

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-let cars = {"Acura" , "Bugatti" ,"Aston martin"}
+// let cars = [{"Acura","Bugatti","Aston martin","Bmw","Mercedes","Lamborghini","MINI","Subaru";
+
+
+// // import cars from './cars.json' assert{type:'json'}
+// // console.log(cars)
+// }
+fetch(ca)
